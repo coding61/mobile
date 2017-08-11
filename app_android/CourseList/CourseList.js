8d333de16602fd68e7b7b8f7890f98e540674623
chen <chen158_7@126.com>
2017-08-11 16:54:44 +0800

    安卓相关/ios 跟

--- app_android/CourseList/CourseList.js
+++ app_android/CourseList/CourseList.js
@@ -241,9 +241,7 @@ class CourseList extends Component {
   //     alert('还未选择课程');
   //   }
   // }
-  componentWillUnmount() {
-    this.props.navigation.state.params.callback();
-  }
+  
   goReset() {
     var _this = this;
     AsyncStorage.getItem("token", function(errs, results) {
