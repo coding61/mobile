7e2c08d107690c3441ff5b5417902d6b25eefc56
lhuazai <18674785073@163.com>
2017-08-21 17:41:41 +0800

    forum ranklist news11229933

--- android/app/src/main/java/com/cxy61/girls/MainApplication.java
+++ android/app/src/main/java/com/cxy61/girls/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 // import com.zmxv.RNSound.BuildConfig;
+import com.tendcloud.tenddata.TCAgent;
 import com.zmxv.RNSound.RNSoundPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -39,5 +40,11 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+    TCAgent.LOG_ON=true;
+    // App ID: 在TalkingData创建应用后，进入数据报表页中，在“系统设置”-“编辑应用”页面里查看App ID。
+    // 渠道 ID: 是渠道标识符，可通过不同渠道单独追踪数据。
+    TCAgent.init(this, "7C7B55DD67C64E80B6708EF3E9BFD401", "tencent_app_market");
+    // 如果已经在AndroidManifest.xml配置了App ID和渠道ID，调用TCAgent.init(this)即可；或与AndroidManifest.xml中的对应参数保持一致。
+    TCAgent.setReportUncaughtExceptions(true);
   }
 }
