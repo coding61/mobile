94afb67eacdb44679fd3175f3221930420120395
chen <chen158_7@126.com>
2017-08-22 12:12:46 +0800

    add 三方 device-info

--- android/app/src/main/java/com/cxy61/girls/MainApplication.java
+++ android/app/src/main/java/com/cxy61/girls/MainApplication.java
@@ -3,6 +3,7 @@ package com.cxy61.girls;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 // import com.zmxv.RNSound.BuildConfig;
 import com.tendcloud.tenddata.TCAgent;
 import com.zmxv.RNSound.RNSoundPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNDeviceInfo(),
             new RNSoundPackage()
       );
     }
