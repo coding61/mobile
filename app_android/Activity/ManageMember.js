684a6b24cfa21a8e86ffd1c4b92febb5da045f4c
varwym <390759142@qq.com>
2017-10-20 14:40:43 +0800

    合并android

---
+++ app_android/Activity/ManageMember.js
@@ -0,0 +1,110 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+import {
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Text,
+  TextInput,
+  ScrollView,
+  Dimensions,
+  FlatList,
+  Image,
+  AsyncStorage,
+  Alert
+} from 'react-native';
+const {width, height} = Dimensions.get('window');
+import Utils from '../utils/Utils.js';
+import Http from '../utils/Http.js';
+class ManageMember extends Component {
+  static navigationOptions  = ({ navigation, screenProps }) => ({
+    title: '管理成员',
+    headerStyle: {
+      backgroundColor: 'rgb(251, 109, 150)'
+    },
+    headerTitleStyle: {
+      color: 'white',
+      fontWeight: '400'
+    },
+    headerTintColor: "#fff"
+  })
+  constructor() {
+  	super();
+  	this.state = {
+      data: null,
+      isUp: false
+  	}
+  }
+  componentWillMount() {
+    this.setState({
+      data: this.props.navigation.state.params.data.club_member
+    })
+  }
+  componentWillUnmount() {
+    this.props.navigation.state.params.callback(this.state.isUp);
+  }
+  delete(pk, index) {
+    var _this = this;
+    Alert.alert('','确认删除？',
+      [{text: '取消', onPress: () => {}, style: 'cancel'},
+      {text: '确认', onPress: () => {
+        AsyncStorage.getItem('token', (errs, results) => {
+          fetch(Http.domain + '/club/delete_clubmember/' + pk + '/',{headers: {'Authorization': 'Token ' + results, 'content-type': 'application/json'}})
+            .then(response => {
+              if (response.ok === true) {
+                return 'success';
+              } else {
+                return response.json();
+              }
+            })
+            .then(res => {
+              if (res === 'success') {
+                var data = this.state.data.concat();
+                data.splice(index, 1);
+                _this.setState({
+                  data: data,
+                  isUp: true
+                })
+                Utils.showMessage('删除成功！');
+              } else {
+                if (res.message) {
+                  Utils.showMessage(res.message);
+                } else {
+                  Utils.showMessage('删除失败！');
+                }
+              }
+            })
+        })
+      }}
+      ],{ cancelable: false })
+  }
+  _keyExtractor = (item, index) => index
+  _renderItem = ({item, index}) => {
+    return  (
+      <View style={{borderBottomWidth: 1, borderBottomColor: 'rgb(242, 243, 244)', width: width, height:60, flexDirection: 'row', alignItems: 'center', backgroundColor: 'white'}}>
+        <Image resizeMode={'contain'} style={{marginLeft: 20, width: 40, height: 40}} source={{uri: item.owner.avatar}}/>
+        <Text style={{marginLeft: 20, fontSize: 16}}>{item.owner.name}</Text>
+        <TouchableOpacity onPress={this.delete.bind(this, item.pk, index)}style={{width: 60, height: 60, alignItems: 'center', justifyContent: 'center', position: 'absolute', right: 0, top: 0}}>
+          <Image resizeMode={'contain'} style={{width: 15, height: 15}} source={require('../assets/My/delete.png')}/>
+        </TouchableOpacity>
+      </View>
+      )
+  }
+  render() {
+    return (
+      <View style={{flex: 1, backgroundColor: 'rgb(243, 243, 243)'}}>
+      	<FlatList 
+          extraData={this.state}
+          data={this.state.data}
+          renderItem={this._renderItem}
+          keyExtractor={this._keyExtractor}
+        />
+      </View>
+    );
+  }
+}
+
+
+export default ManageMember;
---
+++ app_android/My/CatalogCourse.js
@@ -0,0 +1,127 @@
+'use strict';
+import React, { Component } from 'react';
+
+import {
+  StyleSheet,
+  View,
+  ScrollView,
+  Dimensions,
+  TouchableOpacity,
+  Image,
+  Text,
+  AsyncStorage,
+  Alert,
+  DeviceEventEmitter,
+  FlatList
+} from 'react-native';
+const {width, height} = Dimensions.get('window');
+import Http from '../utils/Http.js';
+
+class CatalogCourse extends Component {
+  static navigationOptions  = ({ navigation, screenProps }) => ({
+    title: navigation.state.params.switch,
+    headerStyle: {
+      backgroundColor: 'rgb(250, 80, 131)'
+    },
+    headerTitleStyle: {
+      color: 'white',
+      fontWeight: '400'
+    },
+    headerTintColor: "#fff"
+  })
+  constructor() {
+    super();
+    this.state = {
+      switch: null,
+      dataSource: null,
+      selected: null
+    }
+  }
+  componentWillMount() {
+    var _this = this;
+    AsyncStorage.getItem('token', function(errs, results) {
+      fetch(Http.domain + '/course/courses/', {headers: {Authorization: 'Token ' + results, 'content-type': 'application/json'}})
+        .then(response => {
+          if (response.ok === true) {
+            return response.json();
+          }
+        })
+        .then(responseJSON => {
+          var datas = new Array();
+          if (_this.props.navigation.state.params.switch === '已完成课程') {
+            responseJSON.forEach((item) => {
+              if (item.learn_extent.status === 'finish') {
+                datas.push(item);
+              }
+            })
+          } else {
+            responseJSON.forEach((item) => {
+              if (item.learn_extent.status === 'processing') {
+                datas.push(item);
+              }
+            })
+          }
+          var dataSource = new Array();
+          datas.forEach((item, index) => {
+            var isHas = false;
+            for (let i=0;i<dataSource.length;i++) {
+              if (item.profession === dataSource[i].profession) {
+                dataSource[i].dataArr.push(item);
+                isHas = true;
+                break;
+              }
+            }
+            if (!isHas) {
+              var dataArray = new Array();
+              dataArray.push(item);
+              dataSource.push({"profession": item.profession, "dataArr": dataArray})
+            } 
+          })
+          _this.setState({
+            switch: _this.props.navigation.state.params.switch,
+            dataSource: dataSource,
+            selected: 0
+          })
+        })
+    })
+
+  }
+  _keyExtractor = (item, index) => index
+  _keyExtractor_ = (item, index) => index
+  _renderItem = ({item, index}) => {
+    return (<TouchableOpacity onPress={()=> this.setState({selected: index})} style={{paddingLeft: 10, paddingRight: 10, alignItems: 'center', height: 50, justifyContent: 'center'}}><Text style={[index === this.state.selected?({color: 'rgb(251, 103, 151)'}):(null),{fontSize: 14}]}>{item.profession}</Text></TouchableOpacity>)
+  }
+  _renderItemBottom = ({item, index}) => {
+    return (<View style={{borderRadius: 3, marginTop: 20, marginLeft: 20, backgroundColor: 'rgb(234, 235, 236)', width: width - 40, height: 80}}>
+              <Image style={{width: 60, height: 60, position: 'absolute', left: 10, top: 10}} resizeMode={'contain'} source={{uri: item.images}}/>
+              <View style={{marginLeft: 80, justifyContent: 'space-around', height: 80}}>
+                <Text>{item.name}</Text>
+                <Text style={{color: 'rgb(138, 139, 140)'}}>{this.state.switch === '已完成课程'?('已完成'):('学习中')}</Text>
+              </View>
+            </View>)
+  }
+  render() {
+    return (
+      <View style={{flex: 1, backgroundColor: 'rgb(244, 245, 246)'}}>
+        <View style={{width: width, height: 50, borderBottomColor: 'rgb(236, 237, 238)', borderBottomWidth: 1, backgroundColor: 'white'}}>
+          <FlatList 
+            horizontal={true}
+            extraData={this.state}
+            data={this.state.dataSource}
+            renderItem={this._renderItem}
+            keyExtractor={this._keyExtractor}
+          />
+        </View>
+        {this.state.dataSource?(
+          <FlatList 
+            extraData={this.state}
+            data={this.state.dataSource[this.state.selected].dataArr}
+            renderItem={this._renderItemBottom}
+            keyExtractor={this._keyExtractor_}
+          />):(null)}
+      </View>
+    )
+  }
+}
+
+export default CatalogCourse;
