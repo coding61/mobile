284bbc4a47e8c90769250f2d1166bc1a23ab999d
varwym <390759142@qq.com>
2017-10-20 11:13:53 +0800

    修改背景颜色

--- app_ios/My/MyPage.js
+++ app_ios/My/MyPage.js
@@ -189,7 +189,7 @@ class MyPage extends Component {
   }
   render() {
     return (
-      <View style={{flex: 1}}>
+      <View style={{flex: 1, backgroundColor: 'rgb(243, 243, 243)'}}>
       	<ScrollView>
       		{this.state.isLogin === true?(
       			<View style={{backgroundColor: 'rgb(250, 80, 131)', width: width, height: height / 3, alignItems: 'center', justifyContent: 'center'}}>
@@ -240,7 +240,7 @@ class MyPage extends Component {
       			</View>
       			):(null)}
       	</ScrollView>
-        
+
         <Prompt
           title="修改昵称"
           placeholder={'昵称'}
