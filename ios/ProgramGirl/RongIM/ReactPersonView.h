db0f624a4be21c225dd4b4a1c422f8160a540bf9
chen <chen158_7@126.com>
2017-11-30 09:47:46 +0800

    竞赛加提示

---
+++ ios/ProgramGirl/RongIM/ReactPersonView.h
@@ -0,0 +1,13 @@
+//
+//  ReactPersonView.h
+//  ProgramGirl
+//
+//  Created by chen on 2017/11/29.
+//  Copyright © 2017年 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReactPersonView : UIView
+
+@end
---
+++ ios/ProgramGirl/RongIM/ReactPersonViewVC.h
@@ -0,0 +1,13 @@
+//
+//  ReactPersonViewVC.h
+//  ProgramGirl
+//
+//  Created by chen on 2017/11/29.
+//  Copyright © 2017年 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReactPersonViewVC : UIViewController
+
+@end
